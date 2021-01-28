5f712b74e5b8fdb95c81207d7cb83cfac8f29491
Davey <davey_tan@tutanota.com>
2021-01-28 17:54:11 +0700

    Bug class created

---
+++ src/com/hantam/bugreport/Bug.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hantam.bugreport;
+
+/**
+ *
+ * @author Crestfall
+ */
+public class Bug {
+    
+    private int id;
+    private int tester;
+    private String dateOpen;
+    private int rootCause;
+    private int severity;
+    private int priority;
+    private String summary;
+    private String steps;
+    private String isolation;
+    private int state;
+    private int owner;
+    private String estimateFix;
+    private int phaseDetected;
+    private int phaseRemoved;
+    private String dateClose;
+    private String resolution;
+    
+}
