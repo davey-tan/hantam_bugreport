77afb6abdf0707d4f35a801628766169b750e5ad
Davey <davey_tan@tutanota.com>
2021-01-28 17:54:41 +0700

    Connect class created

---
+++ src/com/hantam/bugreport/connect.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hantam.bugreport;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ivana
+ */
+public class connect {
+
+    static Connection conn = null;
+
+    public static Connection connection() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hantam_bugreport?user=root");
+            System.out.println("Connection success");
+        } catch (Exception ex) {
+            Logger.getLogger(connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+}
